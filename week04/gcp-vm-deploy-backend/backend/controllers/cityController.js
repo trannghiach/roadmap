99ba3fccee8d55bc19153f77e8030d22735b8f7b
22120231 <22120231@student.hcmus.edu.vn>
2025-03-04 11:41:50 +0700

    feat: add new folder to clone on VM to test the gcp deployment

---
+++ week04/gcp-vm-deploy-backend/backend/controllers/cityController.js
@@ -0,0 +1,16 @@
+const City = require('../models/City');
+
+const getRandomCities = async (req, res) => {
+    const size = req.params.size ? parseInt(req.params.size) : 1;
+    console.log('size: ', size);
+    try {
+        const cities = await City.aggregate([{$sample: {size}}]);
+        res.json(cities);
+    } catch(err) {
+        res.status(500).json({message: err.message});
+    }
+};
+
+module.exports = {
+    getRandomCities
+};
