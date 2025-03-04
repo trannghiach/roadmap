f98ff0cc40a7d204f31a48aa14101474f410c52c
22120231 <22120231@student.hcmus.edu.vn>
2025-03-05 06:23:39 +0700

    feat: add endpont /metrics for Prometheus to collect in server.js

--- week04/prometheus-grafana-logging-monitoring/backend/server.js
+++ week04/prometheus-grafana-logging-monitoring/backend/server.js
@@ -3,7 +3,35 @@ const dbConnect = require('./config/db');
 const cityRoutes = require('./routes/cityRoutes');
 const cors = require('cors');
 
+const client = require('prom-client');
+
 const app = express();
+
+// //Configure custom metric: http request counter:
+// const httpRequestCounter = new client.Counter({
+//   name: 'http_request_total',
+//   help: 'Total requests received',
+//   labelNames: ['method', 'route', 'status_code'],
+// });
+
+// //Middleware request counter:
+// app.use((req, res, next) => {
+//   req.on('finish', () => {
+//     httpRequestCounter.inc({ method: req.method, route: req.path, status_code: res.statusCode });
+//   });
+//   next();
+// });
+// Or use the available one:
+
+const collectDefaultMetrics = client.collectDefaultMetrics;
+collectDefaultMetrics();
+
+//Endpoint /metrics for the Prometheus to collect
+app.get('/metrics', async(req, res) => {
+  res.set('Content-Type', client.register.contentType);
+  res.send(await client.register.metrics());
+});
+
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 const allowedOrigins = [
